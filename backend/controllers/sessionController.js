50338c086c28542c7d9cb1ca8bdb9e63c8c4cea4
Batya <subatyais@gmail.com>
2020-05-15 19:02:09 +1000

    session refactor

--- backend/controllers/sessionController.js
+++ backend/controllers/sessionController.js
@@ -1,22 +1,20 @@
 const express = require('express'); 
 const router = express.Router();
-const sessionUtils = require('../utils/sessionUtils');
-const User = require('../models/user');
 const _ = require('lodash');
 
-const userService = require('../services/userService');
+const sessionService = require('../services/sessionService');
 
 // routs
 router.post('/signup', async (req, res) => {
     const user = req.body;
 
     try {
-        const isFound = await userService.findUser(user.email);
+        const isFound = await sessionService.findUser(user.email);
                   
         if(isFound) {
             res.status(409).send({warning: "User with this email already exist. Please Login."});
         } else {
-            const userRecord = await userService.signup(user);
+            const userRecord = await sessionService.signup(user);
                         
             res.status(201).json(userRecord);                 
         }
@@ -29,47 +27,47 @@ router.post('/signup', async (req, res) => {
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
    
-    // try {
-    //     const userRecord = await userService.login(email, password);
-    //     console.log(userRecord);
+    try {
+        const userRecord = await sessionService.login(email, password);
+        console.log(userRecord);
 
-    //     if(!_.isEmpty(userRecord)) {
-    //         res.status(200).json(userRecord);
-    //     } else {
-    //         res.status(404).send({ error: "Email/Password are not correct." })
-    //     }
+        if(!_.isEmpty(userRecord)) {
+            res.status(200).json(userRecord);
+        } else {
+            res.status(404).send({ error: "Email/Password are not correct." })
+        }
         
-    // } catch(err) {
-    //     res.status(500).send({ error: err.message })
-    // }
+    } catch(err) {
+        res.status(500).send({ error: err.message })
+    }
        
 
-    let user
+    // let user
 
-    User.findByEmail(email)
-        .then(userRecord => {
-            if(userRecord.rows.length > 0) {
-                console.log(userRecord);
+    // User.findByEmail(email)
+    //     .then(userRecord => {
+    //         if(userRecord.rows.length > 0) {
+    //             console.log(userRecord);
                 
-                user = userRecord.rows[0];                 
-                return sessionUtils.checkPassword(password, user.password_digest);
-            } else {
-                res.status(404).send({error: "User with this email does not exist."})
-            }
-        })
-        .then(res => {
-            if(res) {
-                return sessionUtils.createToken();
-            } else {
-                res.status(404).send({error: "Password is not correct."})   
-            }
-        })
-        .then(token => User.updateUserToken(user.email, token))
-        .then(updatedUserRecord => {
-            delete user.password_digest;
-            return res.status(200).json({ user: updatedUserRecord.rows[0] });
-        })
-        .catch(err => res.status(500).send({ error: err.message }));
+    //             user = userRecord.rows[0];                 
+    //             return sessionUtils.checkPassword(password, user.password_digest);
+    //         } else {
+    //             return res.status(404).send({error: "User with this email does not exist."})
+    //         }
+    //     })
+    //     .then(res => {
+    //         if(res) {
+    //             return sessionUtils.createToken();
+    //         } else {
+    //             return res.status(404).send({error: "Password is not correct."})   
+    //         }
+    //     })
+    //     .then(token => User.updateUserToken(user.email, token))
+    //     .then(updatedUserRecord => {
+    //         delete user.password_digest;
+    //         return res.status(200).json({ user: updatedUserRecord.rows[0] });
+    //     })
+    //     .catch(err => res.status(500).send({ error: err.message }));
 });
 
 
