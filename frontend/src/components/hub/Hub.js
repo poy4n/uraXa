2f141b359e938d3dc51101a4ba85a5fca4a34fdf
Batya <subatyais@gmail.com>
2020-05-17 21:15:22 +1000

    no auth for post by tags - remove auth, merge hub.js

--- frontend/src/components/hub/Hub.js
+++ frontend/src/components/hub/Hub.js
@@ -1,18 +1,59 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { Map } from '../map/Map';
 import { Sidebar } from '../sidebar/Sidebar';
 import SearchBar from '../search/SearchBar';
+import Filter from '../filterTags/Filter';
+import { markerPosition } from '../map/markerPosition';
 
 import '../map/Map.css';
 
 export default function Hub() {
+	const [ tags, setTags ] = useState([]);
+	const [ markers, setMarkers ] = useState([]);
+
+	useEffect(() => {
+		let url = `/api/post/tags/all`;
+
+		const requestOptions = {
+			method: 'GET',
+			headers: { 'Content-Type': 'application/json' }
+		};
+
+		fetch(url, requestOptions)
+			.then((response) => {
+				const json = response.json();
+				console.log(json);
+				return json;
+			})
+			.then((data) => {
+				console.log(data.postsByTag);
+				setTags(data.postsByTag);
+			})
+			.catch((err) => {
+				console.log(err.error);
+			});
+	}, []);
+
+	useEffect(
+		() => {
+			let marks = markerPosition(tags);
+			console.log(marks);
+			if (marks.length > 0) {
+				setMarkers(marks);
+			}
+		},
+		[ tags ]
+	);
+
 	return (
 		<React.Fragment>
 			<div className='hub'>
 				<Sidebar />
 				<div className='container-map'>
+					{/* <Filter /> */}
 					<SearchBar />
-					<Map />
+					<Map markers={markers} />
 				</div>
 			</div>
 		</React.Fragment>
