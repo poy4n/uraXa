3aadf0dafa1ea694984fb416a5f033589eaab326
Batya <subatyais@gmail.com>
2020-05-23 18:41:57 +1000

    build and server amend to serve static

---
+++ frontend/build/precache-manifest.e909e89e149157c298b4a4ae21a4d3e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "406afce21215d4c01ca9db7bd0d6c594",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65e2f38182d3a56236b3",
+    "url": "/static/css/main.29bb5067.chunk.css"
+  },
+  {
+    "revision": "49f812658d5a4010559e",
+    "url": "/static/js/2.b59212a6.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.b59212a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65e2f38182d3a56236b3",
+    "url": "/static/js/main.f5fd8a1c.chunk.js"
+  },
+  {
+    "revision": "252f58e32352bec2c9c1",
+    "url": "/static/js/runtime-main.482db931.js"
+  }
+]);
